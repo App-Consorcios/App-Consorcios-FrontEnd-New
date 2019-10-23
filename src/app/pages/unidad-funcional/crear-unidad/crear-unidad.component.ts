5371d0435141fa1f0bbb9a0385a818c65ff80df3
hernanArena-UADE <hernan.arena@gmail.com>
2019-10-23 02:13:45 -0300

    cambios unidad

--- src/app/pages/unidad-funcional/crear-unidad/crear-unidad.component.ts
+++ src/app/pages/unidad-funcional/crear-unidad/crear-unidad.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UnidadFuncional } from 'src/app/models/unidad-funcional.model';
 import { ExpensasService } from 'src/app/services/expensas/expensas.service';
 import { UnidadFuncionalService } from 'src/app/services/unidad-funcional/unidad-funcional.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-unidad',
@@ -10,15 +11,53 @@ import { UnidadFuncionalService } from 'src/app/services/unidad-funcional/unidad
 })
 export class CrearUnidadComponent implements OnInit {
   unidadesFuncionales:UnidadFuncional[] = [];
-  
+  unidadFuncional:object = {
+    codigo:"",
+    prorrateo:"",
+    tamanio:"",
+    ubicacion:{
+      codigo: "",
+      descrp: ""
+    }
+  }
+  unidad:string = "";
+  mensaje:boolean = false;
+  forma:FormGroup;
+
   constructor(private _uf:UnidadFuncionalService) {
     this._uf.getUnidades().subscribe( data =>{
       this.unidadesFuncionales = data;
     });
+    this.forma = new FormGroup({
+      'codigo': new FormControl('',[Validators.required,Validators.minLength(3)]),
+      'prorrateo': new FormControl('',Validators.required),
+      'tamanio': new FormControl('',Validators.required),
+      'ubicacion': new FormGroup({
+        'codigo': new FormControl('',[Validators.required,Validators.minLength(3)]),
+        'descrp': new FormControl('')
+      })
+    })
 
   }
 
   ngOnInit() {
+  }
+  guardarCambios(){
+    console.log(this.forma.value);
+    console.log(this.forma);
+    this.mensaje = true;
+    setTimeout(()=>{ this.mensaje = false},3000)
+    this.unidad = this.forma.get('codigo').value;
+
+     this.forma.reset({
+       codigo:"",
+       prorrateo:"",
+       tamanio:"",
+       ubicacion:{
+         codigo: "",
+         descrp: ""
+       }
+     });
   }
 
 
